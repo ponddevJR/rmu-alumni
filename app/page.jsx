c2598396f079c8320b987b2011fe35b08449f263
ponddevJR <663170010324@rum.ac.th>
2025-05-03 13:51:00 +0700

    change timeout

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,7 @@ const FirstPage = () => {
   useEffect(() => {
     const timeout = setInterval(() => {
       setShowDialog(true);
-    }, 5000);
+    }, 4500);
 
     if (showDialog) return clearInterval(timeout);
 
